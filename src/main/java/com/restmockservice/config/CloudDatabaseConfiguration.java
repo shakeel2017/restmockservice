09203e640760d76fd11b1be6ec53c9ef98f45392
Shakeel <sahmed@nisum.com>
2017-09-21 12:40:14 -0700

    Test

--- src/main/java/com/restmockservice/config/CloudDatabaseConfiguration.java
+++ src/main/java/com/restmockservice/config/CloudDatabaseConfiguration.java
@@ -4,14 +4,8 @@ import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-
-import com.mongodb.Mongo;
 
 @Configuration
-@EnableMongoRepositories("com.restmockservice.repository")
 public class CloudDatabaseConfiguration extends AbstractCloudConfig {
 
     @Bean
@@ -19,22 +13,5 @@ public class CloudDatabaseConfiguration extends AbstractCloudConfig {
         return connectionFactory().mongoDbFactory();
     }
     
-    @Bean
-    public ValidatingMongoEventListener validatingMongoEventListener() {
-        return new ValidatingMongoEventListener(validator());
-    }
-    
-    @Bean
-    public LocalValidatorFactoryBean validator() {
-        return new LocalValidatorFactoryBean();
-    }
-    
-    public String getDatabaseName() {
-        return mongoDbFactory().getDb().getName();
-    }
-
-    public Mongo mongo() throws Exception {
-        return mongoDbFactory().getDb().getMongo();
-    }
 
 }
