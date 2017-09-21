6dded31dc343fc54901ae91632624e679323d101
Shakeel <sahmed@nisum.com>
2017-09-20 21:01:54 -0700

    Cloud mongo Config

---
+++ src/main/java/com/restmockservice/config/CloudDatabaseConfiguration.java
@@ -0,0 +1,16 @@
+package com.restmockservice.config;
+
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+
+@Configuration
+public class CloudDatabaseConfiguration extends AbstractCloudConfig {
+
+    @Bean
+    public MongoDbFactory mongoDbFactory() {
+        return connectionFactory().mongoDbFactory();
+    }
+
+}
