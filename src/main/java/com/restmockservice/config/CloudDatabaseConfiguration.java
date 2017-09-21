bcf0b87b3d6bcb66e1ce778827377b4ce1b270d3
Shakeel <sahmed@nisum.com>
2017-09-21 11:20:46 -0700

    Mongo Config

--- src/main/java/com/restmockservice/config/CloudDatabaseConfiguration.java
+++ src/main/java/com/restmockservice/config/CloudDatabaseConfiguration.java
@@ -4,13 +4,37 @@ import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import com.mongodb.Mongo;
 
 @Configuration
+@EnableMongoRepositories("com.restmockservice.repository")
 public class CloudDatabaseConfiguration extends AbstractCloudConfig {
 
     @Bean
     public MongoDbFactory mongoDbFactory() {
         return connectionFactory().mongoDbFactory();
+    }
+    
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+    
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
+    
+    public String getDatabaseName() {
+        return mongoDbFactory().getDb().getName();
+    }
+
+    public Mongo mongo() throws Exception {
+        return mongoDbFactory().getDb().getMongo();
     }
 
 }
--- src/main/java/com/restmockservice/config/CloudMongoDbConfiguration.java
+++ src/main/java/com/restmockservice/config/CloudMongoDbConfiguration.java
@@ -1,41 +1,41 @@
-package com.restmockservice.config;
-
-import javax.inject.Inject;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-
-import com.mongodb.Mongo;
-
-@Configuration
-@EnableMongoRepositories("com.restmockservice.repository")
-public class CloudMongoDbConfiguration extends AbstractMongoConfiguration  {
-	
-	@Inject
-	private MongoDbFactory mongoDbFactory;
-    
-    @Bean
-    public ValidatingMongoEventListener validatingMongoEventListener() {
-        return new ValidatingMongoEventListener(validator());
-    }
-    
-    @Bean
-    public LocalValidatorFactoryBean validator() {
-        return new LocalValidatorFactoryBean();
-    }
-    
-    @Override
-    protected String getDatabaseName() {
-        return mongoDbFactory.getDb().getName();
-    }
-
-    @Override
-    public Mongo mongo() throws Exception {
-        return mongoDbFactory().getDb().getMongo();
-    }
-}
+//package com.restmockservice.config;
+//
+//import javax.inject.Inject;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.mongodb.MongoDbFactory;
+//import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+//import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+//import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+//import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+//
+//import com.mongodb.Mongo;
+//
+//@Configuration
+//@EnableMongoRepositories("com.restmockservice.repository")
+//public class CloudMongoDbConfiguration extends AbstractMongoConfiguration  {
+//	
+//	@Inject
+//	private MongoDbFactory mongoDbFactory;
+//    
+//    @Bean
+//    public ValidatingMongoEventListener validatingMongoEventListener() {
+//        return new ValidatingMongoEventListener(validator());
+//    }
+//    
+//    @Bean
+//    public LocalValidatorFactoryBean validator() {
+//        return new LocalValidatorFactoryBean();
+//    }
+//    
+//    @Override
+//    protected String getDatabaseName() {
+//        return mongoDbFactory.getDb().getName();
+//    }
+//
+//    @Override
+//    public Mongo mongo() throws Exception {
+//        return mongoDbFactory().getDb().getMongo();
+//    }
+//}
